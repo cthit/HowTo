0c0970eb54fcc8674db6925b74b697340dc2f79a
Hoidi <31310934+Hoidi@users.noreply.github.com>
2019-07-30 15:18:50 +0200

    Changes tutorials to be in english

--- src/descriptions/addMail.jsx
+++ src/descriptions/addMail.jsx
@@ -1,8 +1,3 @@
 export const addMail =
-  "Så här lägger man till en **mail**. \n  Jättelätt. Här kommer _massa annan text_.  " +
-  "Linebreak? Vad är det ens? wjkdankjdawnkjdn ajknd jkan wjkn jkawndkjn akjndkj nakjd nkan dkjnawd" +
-  "awjkdnkawndkjawndk.  kjawndkjnawkdjnakjwdnkj" +
-  "" +
-  "akjwndkanwdkjnaw jk djka wdjk awdkj akj djka wdkja  wdjkawkjd kj awkjd kaj d" +
-  "awdkl ka wdk dkj akjdw awkjd" + "<br/>" +
-  " ![alt text](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png \"Logo Title Text 1\")";
+    "This is how you make coffee. \n" +
+    "I don't know how to make coffee, sorry";
