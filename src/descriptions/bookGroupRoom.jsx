ccbd2aa1fd39efa9e48af06c509c9a5ddb493b70
Hoidi <tobiakar@student.chalmers.se>
2019-08-15 16:33:23 +0200

    Adds howTo book a group room

---
+++ src/descriptions/bookGroupRoom.jsx
@@ -0,0 +1,7 @@
+export const bookGroupRoom =
+    "1. Go to [this](https://cloud.timeedit.net/chalmers/web/b1/) website and log in with your Chalmers login details (cid & password). The link can also be found furthest down at the [Student portal](https://student.portal.chalmers.se/)\n" +
+    "2. Click **Book**/**Boka**.\n" +
+    "3. Search for the group room you want to book. You can also choose to only view rooms in certain buildings (Like only the EDIT-building) and choose to only view rooms that are available during certain times.\n" +
+    "4. Click on the room and what time you want to book it. The default time is one hour but this can be changed after clicking.\n" +
+    "5. You have to pick a purpose when booking a room. Pick *Övrigt* because it's the only one available.\n" +
+    "6. Click **Boka**. The room is now booked and you should now get a confirmation that you can send to your group members if you want to.\n";
--- src/descriptions/everything.jsx
+++ src/descriptions/everything.jsx
@@ -1,9 +1,11 @@
 import { howTo } from "./howTo";
 import { makeCoffee } from "./makeCoffee";
 import { addMail } from "./addMail";
+import { bookGroupRoom} from "./bookGroupRoom";
 
 export const everything = {
     howTo: howTo,
     makeCoffee: makeCoffee,
     addMail: addMail,
+    bookGroupRoom: bookGroupRoom,
 };
