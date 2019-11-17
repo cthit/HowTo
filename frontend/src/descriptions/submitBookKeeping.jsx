b65f4f81b7e949a40d587419d7fa62fac6bca413
Latte <gustav.lahti@gmail.com>
2019-11-17 13:49:05 +0100

    Improve formatting and add some info regarding 'huvudbok'.

--- frontend/src/descriptions/submitBookKeeping.jsx
+++ frontend/src/descriptions/submitBookKeeping.jsx
@@ -1,29 +1,43 @@
 export const submitBookKeeping =
     "15 läsdagar innan varje sektionsmöte (oftast torsdag LP3) måste sektionens kassörer skicka ett mail till [revisorerna](mailto:revisorer@chalmers.it) som uppfyller följande instruktioner:  \n" +
-
+    "&nbsp;  \n" +
     "**Ämnesrad vid ordinarie granskning:**  \n" +
-    "`<KOMMITÉNAMN> Sektionsmöte LP<LP> bokföring`  \n" +
+    "'`<KOMMITÉNAMN> Sektionsmöte LP<LP> bokföring`'  \n" +
     "**Ämnesrad vid granskning inför ansvarsbefrielse:**  \n" +
-    "`<KOMMITÉNAMN+MANDATÅR> ansvarsbefrielse LP<LP>`  \n" +
-    "Ex: Säg att FooIT18/19 vill lämna in sin bokföring inför sektionsmötet LP1, då skickar kassören ett mail med ämnesraden  \n" +
-    "`FooIT Sektionsmöte LP1 bokföring`  \n" +
-    "Ex: FooIT18/19 vill i LP2 lämna in sin bokföring för att bli ansvarsbefriade, då skickar kassören ett mail med ämnesraden  \n" +
-    "`FooIT18/19 ansvarsbefrielse LP2`  \n" +
-
+    "'`<KOMMITÉNAMN+MANDATÅR> ansvarsbefrielse LP<LP>`'  \n" +
+    "&nbsp;  \n" +
+    "Ex: Säg att FooIT18 vill lämna in sin bokföring inför sektionsmötet LP1, då skickar kassören ett mail med ämnesraden  \n" +
+    "'`FooIT Sektionsmöte LP1 bokföring`'  \n" +
+    "&nbsp;  \n" +
+    "Ex: FooIT18 vill i LP2 lämna in sin bokföring för att bli ansvarsbefriade, då skickar kassören ett mail med ämnesraden  \n" +
+    "'`FooIT18 ansvarsbefrielse LP2`'  \n" +
+    "&nbsp;  \n" +
     "##### I mailet ska det finnas följande dokument med tillhörande filnamn:\n" +
-    "- **Verifikationslista** (hittas i FortNox): `<KOMMITÉNAMN+VERKSAMHETSÅR>_lp<LP>_verifikat.pdf`\n" +
-    "- **Huvudbok** (hittas i FortNox): `<KOMMITÉNAMN+VERKSAMHETSÅR>_lp<LP>_huvudbok.pdf`\n" +
-    "- **Resultatrapport** (hittas i FortNox): `<KOMMITÉNAMN+VERKSAMHETSÅR>_lp<LP>_resultat.pdf`\n" +
-    "- **Balansrapport** (hittas i FortNox): `<KOMMITÉNAMN+VERKSAMHETSÅR>_lp<LP>_balans.pdf`\n" +
-    "- **Kontoplan** (hittas i FortNox): `<KOMMITÉNAMN+VERKSAMHETSÅR>_lp<LP>_konto.pdf`\n" +
-    "- **Transaktionslista** (hittas i internetbanken): `<KOMMITÉNAMN+VERKSAMHETSÅR>_lp<LP>_transaktion.pdf`\n" +
-    "- **Transaktionslista, Swish** (gäller endast styrIT, hittas i internetbanken): `styrit<VERKSAMHETSÅR>_lp<LP>_swish.pdf`  \n" +
+    "- **Verifikationslista** (hittas i FortNox): '`<KOMMITÉNAMN+VERKSAMHETSÅR>_lp<LP>_verifikat.pdf`'\n" +
+    "- **Huvudbok** (hittas i FortNox): '`<KOMMITÉNAMN+VERKSAMHETSÅR>_lp<LP>_huvudbok.pdf`'\n" +
+    "- **Resultatrapport** (hittas i FortNox): '`<KOMMITÉNAMN+VERKSAMHETSÅR>_lp<LP>_resultat.pdf`'\n" +
+    "- **Balansrapport** (hittas i FortNox): '`<KOMMITÉNAMN+VERKSAMHETSÅR>_lp<LP>_balans.pdf`'\n" +
+    "- **Kontoplan** (hittas i FortNox): '`<KOMMITÉNAMN+VERKSAMHETSÅR>_lp<LP>_konto.pdf`'\n" +
+    "- **Transaktionslista** (hittas i internetbanken): '`<KOMMITÉNAMN+VERKSAMHETSÅR>_lp<LP>_transaktion.pdf`'\n" +
+    "- **Transaktionslista, Swish** (gäller endast styrIT, hittas i internetbanken): '`styrit<VERKSAMHETSÅR>_lp<LP>_swish.pdf`'\n\n" +
 
-    "Om du skulle behöva skicka flera filer under samma kategori så lägger du till ett index i slutet på namnet.\n" +
+    "Om du skulle behöva skicka flera filer under samma kategori så lägger du till perioden som dokumentet beskriver i slutet på namnet.  \n" +
     "Ex: BarIT19 har ett verksamhetsår som börjar under styrIT18s verksamhetsår och slutar under styrIT19s verksamhetsår.\n" +
     "Då behöver kassören skicka in två huvudböcker för att täcka in hela sitt verksamhetsår efter att styrIT19 gått på.\n" +
-    "Då kommer de heta `barit19_lp1_huvudbok_0.pdf` och `barit19_lp1_huvudbok_0.pdf` i LP1.\n" +
-
+    "Då kommer de heta  \n" +
+    "'`barit19_lp1_huvudbok_lp3lp4.pdf`'  \n" +
+    "och  \n" +
+    "'`barit19_lp1_huvudbok_lp1.pdf`'  \n" +
+    "i LP1.  \n" +
+    "&nbsp;  \n" +
+    "Ibland är det även relevant att lägga till huvudboken för hela sektionen, exempelvis ifall en kommité har bokfört en förändring mot an annan kommités bankkonto.\n" +
+    "Huvudboken för hela sektionen ska då heta  \n" +
+    "'`sektionen<STYRITS_VERKSAMHETSÅR>_lp<LP>_huvudbok.pdf`'.  \n" +
+    "Ex: BarIT19 har fått pengar från FooIT19 för att hålla i en sittning som FooIT bokför under LP1.\n" +
+    "När BarIT19 då lämnar in sin bokföring inför sektionsmötet LP3 ska även sektionens huvudbok skickas med under namnet  \n" +
+    "'`sektionen19_lp1_huvudbok.pdf`'  \n" +
+    "utöver de vanliga dokumenten.  \n" +
+    "&nbsp;  \n" +
     "##### Bra att tänka på:\n" +
     "- När granskningen sker ska fysisk bokföring (verifikat, kvitton, deltagarlistor, internfordringar, samt fakturor) finnas tillgängligt för revisorerna. Verifikaten bör även vara underskivna, detta är särskilt viktigt inför ansvarsbefrielse.\n" +
     "- Om kassören av någon anledning ligger efter i bokföringen inför granskningen ska bokföring ändå skickas in till den grad det går samt så behöver revisorerna få veta att kassören ligger efter samt varför.\n" +
@@ -34,4 +48,4 @@ export const submitBookKeeping =
     "- Kolla så att bankkontot stämmer överens mellan transaktionslistan och huvudboken för hela sektionen då transaktioner kan bokföras på olika kostnadsställen och om de inte gör det lägg en kommentar på varför de inte gör det.\n" +
     "- Kolla så att ni har följt ekonomisk policy gällande intern representation, extern representation, aspning, profilering, samt överlämning.\n" +
     "  - Om ni fått dispens för någon av dessa se till att det finns en kommentar i verifikatet där ni refererar till styrelsemötet.\n" +
-    "- Att det finns deltagarlistor på tillfällen som det har bjudits på mat (ex teambuilding och aspning)";
+    "- Om det finns deltagarlistor på tillfällen som det har bjudits på mat (ex teambuilding och aspning)";
