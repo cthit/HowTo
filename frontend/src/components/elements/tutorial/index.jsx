4f9593832e50177071ca3e95d31c23f0368c7dea
Linnea Johansson <43148111+streckgumman@users.noreply.github.com>
2022-02-13 11:07:57 +0100

    added open and close-button

--- frontend/src/components/elements/tutorial/index.jsx
+++ frontend/src/components/elements/tutorial/index.jsx
@@ -8,6 +8,7 @@ import {
     DigitLayout,
     DigitMarkdown,
     useDigitCustomDialog,
+    DigitButton
 } from "@cthit/react-digit-components";
 
 const Service = ({
@@ -19,13 +20,16 @@ const Service = ({
     const [openDialog] = useDigitCustomDialog({
         title: "",
     });
+
     return (
-        <DigitDesign.Card size={{ width: "400px", height: "200px" }}>
+        <DigitDesign.Card size={{ width: "400px" }}>
             <DigitDesign.CardBody
                 style={{ cursor: "pointer" }}
                 onClick={() => {
                     openDialog({
-                        renderButtons: () => null,
+                        renderButtons: (_, cancel) => (
+                            <DigitButton text="Close" onClick={cancel} />
+                        ),
                         renderMain: () => (
                             <>
                                 <DigitLayout.Row>
@@ -44,11 +48,14 @@ const Service = ({
                     });
                 }}
             >
-                <DigitLayout.Row>
-                    <TutorialIcon className={"fa " + fontawesome_icon} />
-                    <DigitText.Title text={title} />
-                </DigitLayout.Row>
-                <DigitMarkdown markdownSource={description} />
+                <DigitLayout.Column flex="1" justifyContent="space-between">
+                    <DigitLayout.Row>
+                        <TutorialIcon className={"fa " + fontawesome_icon} />
+                        <DigitText.Title text={title} />
+                    </DigitLayout.Row>
+                    <DigitMarkdown markdownSource={description} />
+                    <DigitButton text="Open" />
+                </DigitLayout.Column>
             </DigitDesign.CardBody>
         </DigitDesign.Card>
     );
